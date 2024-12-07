1ae6d539cbbb8adcb57d1b4c0a9ecf5c50168304
PrarthanAgarwal <prarthanagawraljeen@gmail.com>
2024-12-07 19:52:12 +0530

    Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import { Home } from './pages/Home';
 import { TermsAndPrivacy } from './pages/TermsAndPrivacy';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/terms-privacy" element={<TermsAndPrivacy />} />
       </Routes>
-    </BrowserRouter>
+      <Analytics />
+    </Router>
   );
 }
 
